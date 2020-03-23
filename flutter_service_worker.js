6ac12465445c03e5984976da91036b34969ba8c0
lecocchi <leju1712@gmail.com>
2020-03-23 16:24:50 -0300

    version 1.0.5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "feda529323f56744bf1c1df1e9df9f8e",
-"/main.dart.js": "f16e5631af17c55da3d6640ee1b46b3d",
+"/main.dart.js": "57d95167c1a05bee4eddffc97b9110a9",
 "/manifest.json": "1f92c99c36969b0467528859f957607e"
 };
 
